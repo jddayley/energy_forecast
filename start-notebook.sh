528a5ce6e342b05dd04062335eace78437f11638
jddayley <jddayley@gmail.com>
2021-11-16 16:50:17 -0500

    Update start-notebook.sh

--- start-notebook.sh
+++ start-notebook.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-exec jupyter notebook --allow-root --ip 0.0.0.0 --no-browser --NotebookApp.token='' --NotebookApp.password=''  &> /dev/null &
-docker run -i -t -p 8888:8888 energyforecast /bin/bash
-docker build -t energyforecast . 
+exec jupyter notebook --allow-root --ip 0.0.0.0 --no-browser --NotebookApp.token='' --NotebookApp.password=''  
